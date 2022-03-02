e458092dcc3a6416717a54a5afaa49089b417258
Chantelle Chan <chantelletchan@gmail.com>
2022-03-01 23:08:29 -0800

    login page and basic routing, added proxy to localhost backend for dev server

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,19 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../services/AxiosService";
 
-const Login = () => {
+const handleLogin = async (email: string, pw: string, setLoginErr: Function, nav: Function) => {
+
+    try {
+
+        // attempt login
+        let res = await axiosInstance.post(
+            `/auth/login`,
+            { email: email, password: pw },
+        );
+        
+        // store user token in temporary session storage
+        // TODO: figure out where to store this in capacitor on android
+        sessionStorage.setItem('userToken', res.data.token);
+
+        // navigate to home
+        nav('/');
+
+    } catch (error) {
+        console.error(error);
+        setLoginErr('Error Logging In')
+    }
+
+}
+
+const ErrorMessage = (props: any) => {
     return (
         <React.Fragment>
+            <div className="alert alert-danger mb-3" role="alert">
+                {props.loginErr}
+            </div>
+        </React.Fragment>
+    )
+}
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPw] = useState('');
+    const [loginErr, setLoginErr] = useState('');
+
+    const nav = useNavigate();
+
+    return (
+        <React.Fragment>
+            {
+                loginErr !== '' && <ErrorMessage loginErr={loginErr} />
+            }
             <div className="text-center mt-5 pt-5">
                 <div className="row">
-                    <div className="offset-md-3 col-xs-12 col-md-6">
+                    <div className="col-xs-12 offset-md-3 col-md-6 offset-lg-4 col-lg-4">
                         <form>
                             <h1 className="h3 mb-3 fw-normal">Sign In Page</h1>
-                            <div className="form-floating">
-                                <input type="email" className="form-control" id="floatingInput" placeholder="name@email.com" />
+                            <div className="form-floating mb-3">
+                                <input
+                                    type="email" className="form-control" id="floatingInput" placeholder="name@email.com"
+                                    value={email} onChange={(e) => setEmail(e.target.value)}
+                                />
                                 <label htmlFor="floatingInput">Email address</label>
                             </div>
-                            <div className="form-floating">
-                                <input type="password" className="form-control" id="floatingPassword" placeholder="password" />
+                            <div className="form-floating mb-3">
+                                <input
+                                    type="password" className="form-control" id="floatingPassword" placeholder="password"
+                                    value={password} onChange={(e) => setPw(e.target.value)}
+                                />
                                 <label htmlFor="floatingPassword">Password</label>
                             </div>
                             {/*                             
@@ -24,7 +75,12 @@ const Login = () => {
                                     </label>
                                 </div> 
                             */}
-                            <button className="w-100 btn btn-lg btn-primary" type="submit"data-form-type="action,login">Sign in</button>
+                            <button
+                                type="button" className="w-100 btn btn-outline-primary"
+                                onClick={() => { handleLogin(email, password, setLoginErr, nav) }}
+                            >
+                                Sign In
+                            </button>
                         </form>
                     </div>
                 </div>
