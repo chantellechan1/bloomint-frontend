eb248accdbbadbbed3aef5bea52b241f95b4396a
Chantelle Chan <chantelletchan@gmail.com>
2022-03-28 19:27:57 -0700

    finished settings page, moved logout to settings

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,11 @@ import {
     RiPlantFill, RiPlantLine,
     RiLeafLine, RiLeafFill,
     RiSettings4Line, RiSettings4Fill,
-    RiLogoutBoxLine
 } from "react-icons/ri";
 import { IconContext } from "react-icons";
 
-import logo from './logo.svg';
 import './App.css';
 
-import Home from './components/Home';
 import Login from './components/Login';
 import PlantTypes from './components/PlantTypes';
 import PlantsAll from './components/PlantsAll';
@@ -68,29 +65,30 @@ function App() {
 
     return (
         <div className="App">
-            {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        App Header
-      </header> */}
-
             <Router>
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<PlantTypes />} />
                     <Route path="/plants_by_type" element={<PlantTypes />} />
                     <Route path="/plants_by_type/:plantTypeID" element={<PlantsInType />} />
                     <Route path="/plant/:plantID" element={<PlantIndividual />} />
                     <Route path="/plant/:plantID/edit" element={<PlantIndividualEdit />} />
                     <Route path="/all_plants" element={<PlantsAll />} />
-                    <Route path="/settings" element={<Settings />} />
+                    <Route path="/settings" element={<Settings handleLogout={handleLogout}/>} />
                 </Routes>
 
                 {/* spacer for bottom nav bar */}
-                {/* TODO: have icons fill onclick, and be line styled icons otherwise */}
                 <div style={{ height: '80px' }}></div>
+
                 {/* bottom nav bar */}
                 <nav className="navbar fixed-bottom navbar-light bg-light">
                     <div className="container-fluid">
-                        <Link to="/" className="navbar-brand">Bloom</Link>
+                        <Link to="/" className="navbar-brand"
+                            onClick={() => {
+                                setSelectedBottomNav('plantsByType')
+                            }}
+                        >
+                            Bloom
+                        </Link>
                         <Link to="/plants_by_type" className="nav-link active" aria-current="page"
                             onClick={() => {
                                 setSelectedBottomNav('plantsByType')
@@ -142,13 +140,6 @@ function App() {
                                 </div>
                             </IconContext.Provider>
                         </Link>
-                        <button className="btn btn-outline-primary" onClick={() => { handleLogout() }}>
-                            <IconContext.Provider value={{ size: "2em" }}>
-                                <div>
-                                    <RiLogoutBoxLine />
-                                </div>
-                            </IconContext.Provider>
-                        </button>
                     </div>
                 </nav>
             </Router>
--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,12 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { RiLogoutBoxLine } from "react-icons/ri";
+import LoadingComponent from "./Loading";
+
+const axoisOptions = {
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('userToken')}`
+    }
+};
 
 const Settings = (props: any) => {
 
-    return (
-        <React.Fragment>
-            Settings component
+    const handleLogout = props.handleLogout;
+    const [loading, setLoading] = useState(true);
+    const [userInfo, setUserInfo] = useState({ email: '', created_at: '' });
 
-        </React.Fragment>
+    useEffect(() => {
+        const getUserInfo = async () => {
+            const res = await axios.get('/auth/get_user', axoisOptions);
+
+            setUserInfo(res.data);
+            setLoading(false);
+        }
+
+        getUserInfo();
+    }, []);
+
+
+    return (
+        loading ?
+            <React.Fragment>
+                <div className='mt-5'>
+                    <LoadingComponent />
+                </div>
+            </React.Fragment>
+            :
+            <React.Fragment>
+                <div className="container-fluid">
+                    <div className="row text-center mb-3 mt-1">
+                        <h1>Settings</h1>
+                    </div>
+                    <div className="row mb-3">
+                        <div className="col">
+                            {/* TODO: implement get user information function */}
+                            <h3>{userInfo.email}</h3>
+                            Joined Bloom: 
+                            <br />
+                            {userInfo.created_at}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <button className="btn btn-outline-danger" onClick={() => { handleLogout() }}>
+                                <RiLogoutBoxLine /> Log out
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
     )
 };
 
