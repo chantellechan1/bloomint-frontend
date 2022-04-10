000932afafeec28dfa25cf40c491b80025b65f00
Chantelle Chan <chantelletchan@gmail.com>
2022-04-10 11:51:34 -0700

    refactor to allow for routes accessed by non-logged in users

--- src/App.tsx
+++ src/App.tsx
@@ -61,111 +61,119 @@ function App() {
         setUserToken(localStorage.getItem('userToken'));
     }
 
-    if (checkLoggedIn() === false) {
-        return <Login userToken={userToken} setUserToken={setUserToken} />
-    }
-
     return (
-        <div className="App">
+        checkLoggedIn() ?
+            // routes for only logged in users
+            <div className="App">
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<PlantTypes />} />
+                        <Route path="/plants_by_type" element={<PlantTypes />} />
+                        <Route path="/plants_by_type/:plantTypeID" element={<PlantsInType />} />
+                        <Route path='/plant/new' element={<PlantIndividualNew />} />
+                        <Route path="/plant/:plantID" element={<PlantIndividual />} />
+                        <Route path="/plant/:plantID/edit" element={<PlantIndividualEdit />} />
+                        <Route path="/all_plants" element={<PlantsAll />} />
+                        <Route path="/settings" element={<Settings handleLogout={handleLogout} />} />
+                    </Routes>
+
+                    {/* spacer for bottom nav bar */}
+                    <div style={{ height: '80px' }}></div>
+
+                    {/* bottom nav bar */}
+                    <nav className="navbar fixed-bottom navbar-light bg-light">
+                        <div className="container-fluid">
+                            <Link to="/" className="navbar-brand"
+                                onClick={() => {
+                                    setSelectedBottomNav('plantsByType')
+                                }}
+                            >
+                                Bloom
+                            </Link>
+                            <Link to="/plants_by_type" className="nav-link active" aria-current="page"
+                                onClick={() => {
+                                    setSelectedBottomNav('plantsByType')
+                                }}
+                            >
+                                <IconContext.Provider value={{ size: "2em" }}>
+                                    <div>
+                                        {
+                                            selectedBottomNav === 'plantsByType'
+                                                ?
+                                                <RiLeafFill />
+                                                :
+                                                <RiLeafLine />
+                                        }
+                                    </div>
+                                </IconContext.Provider>
+                            </Link>
+                            <Link to="/all_plants" className="nav-link"
+                                onClick={() => {
+                                    setSelectedBottomNav('allPlants')
+                                }}
+                            >
+                                <IconContext.Provider value={{ size: "2em" }}>
+                                    <div>
+                                        {
+                                            selectedBottomNav === 'allPlants'
+                                                ?
+                                                <RiPlantFill />
+                                                :
+                                                <RiPlantLine />
+                                        }
+                                    </div>
+                                </IconContext.Provider>
+                            </Link>
+
+                            <Link to="/plant/new" className="nav-link"
+                                onClick={() => {
+                                    setSelectedBottomNav('new')
+                                }}
+                            >
+                                <IconContext.Provider value={{ size: "2em" }}>
+                                    <div>
+                                        {
+                                            selectedBottomNav === 'new'
+                                                ?
+                                                <RiAddCircleFill />
+                                                :
+                                                <RiAddCircleLine />
+                                        }
+                                    </div>
+                                </IconContext.Provider>
+                            </Link>
+
+                            <Link to="/settings" className="nav-link"
+                                onClick={() => {
+                                    setSelectedBottomNav('settings')
+                                }}
+                            >
+                                <IconContext.Provider value={{ size: "2em" }}>
+                                    <div>
+                                        {
+                                            selectedBottomNav === 'settings'
+                                                ?
+                                                <RiSettings4Fill />
+                                                :
+                                                <RiSettings4Line />
+                                        }
+                                    </div>
+                                </IconContext.Provider>
+                            </Link>
+                        </div>
+                    </nav>
+                </Router>
+            </div>
+
+            :
+            // routes that non-logged-in users can access
             <Router>
                 <Routes>
-                    <Route path="/" element={<PlantTypes />} />
-                    <Route path="/plants_by_type" element={<PlantTypes />} />
-                    <Route path="/plants_by_type/:plantTypeID" element={<PlantsInType />} />
-                    <Route path='/plant/new' element={<PlantIndividualNew />} />
-                    <Route path="/plant/:plantID" element={<PlantIndividual />} />
-                    <Route path="/plant/:plantID/edit" element={<PlantIndividualEdit />} />
-                    <Route path="/all_plants" element={<PlantsAll />} />
-                    <Route path="/settings" element={<Settings handleLogout={handleLogout}/>} />
+                    {/* <Route path="create_account" element={ } /> */}
+                    {/* put all routes for not logged in users above this final catch all route */}
+                    <Route path="*" element={<Login userToken={userToken} setUserToken={setUserToken} />} />
                 </Routes>
-
-                {/* spacer for bottom nav bar */}
-                <div style={{ height: '80px' }}></div>
-
-                {/* bottom nav bar */}
-                <nav className="navbar fixed-bottom navbar-light bg-light">
-                    <div className="container-fluid">
-                        <Link to="/" className="navbar-brand"
-                            onClick={() => {
-                                setSelectedBottomNav('plantsByType')
-                            }}
-                        >
-                            Bloom
-                        </Link>
-                        <Link to="/plants_by_type" className="nav-link active" aria-current="page"
-                            onClick={() => {
-                                setSelectedBottomNav('plantsByType')
-                            }}
-                        >
-                            <IconContext.Provider value={{ size: "2em" }}>
-                                <div>
-                                    {
-                                        selectedBottomNav === 'plantsByType'
-                                            ?
-                                            <RiLeafFill />
-                                            :
-                                            <RiLeafLine />
-                                    }
-                                </div>
-                            </IconContext.Provider>
-                        </Link>
-                        <Link to="/all_plants" className="nav-link"
-                            onClick={() => {
-                                setSelectedBottomNav('allPlants')
-                            }}
-                        >
-                            <IconContext.Provider value={{ size: "2em" }}>
-                                <div>
-                                    {
-                                        selectedBottomNav === 'allPlants'
-                                            ?
-                                            <RiPlantFill />
-                                            :
-                                            <RiPlantLine />
-                                    }
-                                </div>
-                            </IconContext.Provider>
-                        </Link>
-
-                        <Link to="/plant/new" className="nav-link"
-                            onClick={() => {
-                                setSelectedBottomNav('new')
-                            }}
-                        >
-                            <IconContext.Provider value={{ size: "2em" }}>
-                                <div>
-                                    {
-                                        selectedBottomNav === 'new'
-                                            ?
-                                            <RiAddCircleFill />
-                                            :
-                                            <RiAddCircleLine />
-                                    }
-                                </div>
-                            </IconContext.Provider>
-                        </Link>
-
-                        <Link to="/settings" className="nav-link"
-                            onClick={() => {
-                                setSelectedBottomNav('settings')
-                            }}
-                        >
-                            <IconContext.Provider value={{ size: "2em" }}>
-                                <div>
-                                    {
-                                        selectedBottomNav === 'settings'
-                                            ?
-                                            <RiSettings4Fill />
-                                            :
-                                            <RiSettings4Line />
-                                    }
-                                </div>
-                            </IconContext.Provider>
-                        </Link>
-                    </div>
-                </nav>
             </Router>
-        </div>
     );
 }
 
