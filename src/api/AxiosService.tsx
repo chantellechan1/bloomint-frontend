eb97d5ca9a195a4fba5ec6a1a346353856d64a33
alexmac22347 <alexmac22347@protonmail.com>
2023-09-25 06:06:00 +0900

    css fix thanks to hugo and change default address

--- src/api/AxiosService.tsx
+++ src/api/AxiosService.tsx
@@ -10,10 +10,10 @@ const axiosOptions: Options = {
   headers: {
     'Content-Type': 'application/json'
   },
-  baseURL: 'http://localhost:3009'
   // when deploying, uncomment this
   // TODO: find a better way to do this
   // baseURL: 'https://bloomint.xyz:3009'
+  baseURL: 'http://127.0.0.1:3009'
 }
 
 export const getOptions = (): Options => axiosOptions
