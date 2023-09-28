76a06965eda006d3fec7a32418852610b693d890
alexmac22347 <alexmac22347@protonmail.com>
2023-09-28 10:41:20 +0900

    loading icon for user plants

--- src/screens/UserPlants.tsx
+++ src/screens/UserPlants.tsx
@@ -5,15 +5,18 @@ import UserPlantRow from '../components/UserPlantRow'
 import { useNavigate } from 'react-router-dom'
 import { RiAddLine } from 'react-icons/ri'
 import FoliageImage from '../assets/images/foliage.png'
+import Loading from '../components/Loading'
 
 const UserPlants = (props: { setUserPlantToEdit: (userPlantToEdit: UserPlant) => void }): JSX.Element => {
   const [userPlants, setUserPlants] = useState<UserPlant[]>([])
+  const [loadedUserPlantsFromServer, setLoadedUserPlantsFromServer] = useState<boolean>(false)
   const navigate = useNavigate()
 
   useEffect(() => {
     const getUserPlants = async (): Promise<void> => {
       try {
         const userPlants: UserPlant[] = await GetUserPlants()
+        setLoadedUserPlantsFromServer(true)
         setUserPlants(userPlants)
       } catch (e) {
         console.error(e)
@@ -33,7 +36,10 @@ const UserPlants = (props: { setUserPlantToEdit: (userPlantToEdit: UserPlant) =>
       <div className="padded-div">
         <p>My plants</p>
       </div>
-      <ul>{userPlantRows}</ul>
+      {loadedUserPlantsFromServer
+        ? (<ul>{userPlantRows}</ul>)
+        : (<div className="centered-div"><Loading/></div>)
+      }
       <button
         className="userplant__add-button"
         onClick={ () => { navigate('./plant_types') }}>
