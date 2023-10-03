0c450b4a8bbaa7add0934a70dbdd6221fcfc554e
alexmac22347 <alexmac22347@protonmail.com>
2023-10-03 18:13:57 +0900

    loading wheel for add plant type

--- src/screens/PlantTypes.tsx
+++ src/screens/PlantTypes.tsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { type PlantType } from '../models/PlantModels'
 import PlantTypeRow from '../components/PlantTypeRow'
 import { GetPlantTypes } from '../api/ServerCalls'
+import Loading from '../components/Loading'
 
 const PlantTypes = (props: { setPlantTypeToAdd: (id: PlantType) => void }): JSX.Element => {
   const [plantTypes, setPlantTypes] = useState<PlantType[]>([])
+  const [loadedPlantTypesFromServer, setLoadedPlantTypesFromServer] = useState<boolean>(false)
 
   // Get all planttypes from server
   useEffect(() => {
@@ -12,6 +14,7 @@ const PlantTypes = (props: { setPlantTypeToAdd: (id: PlantType) => void }): JSX.
       try {
         const plantTypes: PlantType[] = await GetPlantTypes()
         setPlantTypes(plantTypes)
+        setLoadedPlantTypesFromServer(true)
       } catch (e) {
         console.error(e)
       }
@@ -26,11 +29,15 @@ const PlantTypes = (props: { setPlantTypeToAdd: (id: PlantType) => void }): JSX.
 
   return (
     <React.Fragment>
-      <div
-        className="padded-div">
-      Select a plant type to add
+      <div className="padded-div">
+        Select a plant type to add
       </div>
-      <ul>{plantTypeRows}</ul>
+      {loadedPlantTypesFromServer
+        ? (<>
+          <ul>{plantTypeRows}</ul>
+        </>)
+        : (<div className="centered-div userplant__row-container"><Loading/></div>)
+      }
     </React.Fragment>
   )
 }
